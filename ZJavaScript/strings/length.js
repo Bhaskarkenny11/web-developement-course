265aed19e4fe88217aaaa1418bf82fedac01d6b3
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-09-10 16:13:35 +0530

    added strings in html named length.js to length.js.html

--- ZJavaScript/strings/length.js
+++
@@ -1,6 +0,0 @@
-var name="kaat"
-count=name.length
-console.log(count)
-// allowed=140
-// remaining=allowed-count
-console.log("you have entered "+ count +" characters you still Have "+ (140-count)+" characters left")
