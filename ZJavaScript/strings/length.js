96ae0b201fb4ba6c479ae3807d881772db99d381
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-09-10 15:09:33 +0530

    added strings .length variables and concatenation

---
+++ ZJavaScript/strings/length.js
@@ -0,0 +1,6 @@
+var name="kaat"
+count=name.length
+console.log(count)
+// allowed=140
+// remaining=allowed-count
+console.log("you have entered "+ count +" characters you still Have "+ (140-count)+" characters left")
