173b4c446fb4aa7b0e4e5e1586db080ac1cfb8e2
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-09-12 11:04:38 +0530

    post increment and pre increment

---
+++ ZJavaScript/numbers/postincremnt-preincrement.js
@@ -0,0 +1,12 @@
+var x=3
+var y=x++ //this is postincrment so y is assigned the current value of x (3), and then x is incremented to 4.
+console.log("value of y before increment is "+y)
+y+=1
+console.log("value of y after increment is "+y)
+
+console.log("this is same example of preincrement")
+var a=3
+var b= ++a //a is incremented first, so b is assigned the value 4.
+console.log("this is b after pre-increment ")
+b+=1
+console.log("this  is after increment "+b)
