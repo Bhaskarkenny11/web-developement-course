f53ceae08129a34cea9fca15d9583fb71f6565da
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-10-03 17:29:41 +0530

    dom manipulation basics

--- ZJavaScript/DOM Challenge Starting Files/index.js
+++ ZJavaScript/DOM Challenge Starting Files/index.js
@@ -1,3 +1,11 @@
 //document.query
 
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Tiger";
+document.querySelector("button").classList.add("huge")
+document.querySelector("h1").classList.add("huge")
+document.querySelector("button").style.backgroundColor="black"
+document.querySelector("button").style.color="yellow"
+document.querySelector("li a").style.color="green"
+document.querySelector("#title").innerHTML="Back To Normal"
+document.getElementById("title")
+document.querySelector("li a").setAttribute("href" ,"https://www.bing.com")//setAttribute method
