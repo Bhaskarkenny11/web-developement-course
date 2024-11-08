be2ce8757214d08ff8d697c0b7a4df2b8a383742
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-11-08 11:04:44 +0530

    arrow functions even

---
+++ ZJavaScript/numbers/arrowEvenfun.js
@@ -0,0 +1,4 @@
+const fun=(a)=>a%2===0 ? "even":"odd"
+
+
+console.log(fun(2))
---
+++ ZJavaScript/numbers/evenloopRange.js
@@ -0,0 +1,8 @@
+function EvenNumsInRange(a){
+    let evenums=[]
+    for(let i=1;i<=a;i++)
+        if(i%2===0)
+            evenums.push(i)
+ console.log(evenums)
+        }
+        EvenNumsInRange(20)
