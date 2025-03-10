4f0f44196dd01cd0bf2317cf9242c70e93a351e3
Bhaskar Yerrolla <Bhaskar.Yerrolla@genzeon.com>
2025-03-11 00:23:52 +0530

    3.5 secrets project

--- ZZBACKEND/3.4 Middleware/index3.js
+++ ZZBACKEND/3.4 Middleware/index3.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const logger=(req,res,next)=>{
+  console.log("req.method:", req.method);
+  console.log("req.url:", req.url);
+  next();
+}
 
 app.use(logger);
 
---
+++ ZZBACKEND/3.5 Secrets Project/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app=express();
+const port=3000;
+
+ var userIsVerified=false;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+function check(req,res,next){
+    const password=req.body["password"];
+   
+    if (password==="1234"){
+        userIsVerified=true;}
+
+    next();
+}
+
+ app.use(check);
+
+ app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html");
+    
+ });
+
+ app.post("/check",check,(req,res)=>{
+   if(userIsVerified){
+    res.sendFile(__dirname+"/public/secret.html");
+        }
+        else{ res.sendFile(__dirname+"/public/index.html");}
+ 
+ });
+
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
