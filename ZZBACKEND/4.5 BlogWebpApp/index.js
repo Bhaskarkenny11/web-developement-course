3e5fc21187d71738b46d7ebbb028851729a9a249
BhaskarKenny11 <bhaskarshines11@gmail.com>
2025-06-06 11:18:47 +0530

    addded partils and index js and inde ejs

--- ZZBACKEND/4.5 BlogWebpApp/index.js
+++ ZZBACKEND/4.5 BlogWebpApp/index.js
@@ -2,9 +2,17 @@ import express from "express"
 const app=express();
 const port=3000;
 
+import path from 'path';
+import { fileURLToPath } from 'url'; // Import fileURLToPath
+import { dirname } from 'path';    // Import dirname from path
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // Make sure you have this line as well!
+const blog=[{id:1, title:'test'}]
+
 
 app.get('/',(req,res)=>{
-    res.render("index.ejs")
+    res.render('index')
 })
 
 
