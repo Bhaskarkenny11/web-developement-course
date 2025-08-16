9dc8e063ff1c6cabc256c8dd8a3a20c7823e8ed4
BhaskarKenny11 <bhaskarshines11@gmail.com>
2025-08-16 21:00:58 +0530

    added array to store blogs and rotes

--- ZZBACKEND/4.5 BlogWebpApp/index.js
+++ ZZBACKEND/4.5 BlogWebpApp/index.js
@@ -8,13 +8,24 @@ import { dirname } from 'path';    // Import dirname from path
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); // Make sure you have this line as well!
-const blog=[{id:1, title:'test'}]
+const blog=[
+    {id:1, title:'test'},
+    {id:2, title: 'test2'}
+           ]
 
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.render('index',{blog});
+});
+app.get('/pages/:id',(req,res)=>{
+    const postId=req.params.id;
+    const post=blog.find((post)=> post.id==postId);
+    if (post){
+        res.render(`pages/post_${post.id}`,{post});
+    }else{
+        res.status(404).send('Not found');
+    }
 })
-
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`)
